f2717af315e9af44b5c365020b4bea888a4fac32
MaithiL4 <MaithiL4@users.noreply.github.com>
2025-10-01 09:51:27 +0000

    deploy: 2e7ca767fb966175c724a00004e1cf6202ea505a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3077830124"
+    serviceWorkerVersion: "3610923592"
   }
 });
